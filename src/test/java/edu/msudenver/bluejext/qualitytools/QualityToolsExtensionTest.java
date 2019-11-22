58eed0c8b3f7c047c1bcf683b988c3fa09cf30a8
Derek M Holsapple <derekhols31@gmail.com>
2019-11-21 19:37:02 -0700

    Removed tests, will write a acceptance test

--- src/test/java/edu/msudenver/bluejext/qualitytools/QualityToolsExtensionTest.java
+++ src/test/java/edu/msudenver/bluejext/qualitytools/QualityToolsExtensionTest.java
@@ -1,10 +1,8 @@
 package edu.msudenver.bluejext.qualitytools;
 
-import bluej.extensions.BlueJ;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -50,19 +48,4 @@ class QualityToolsExtensionTest {
         URL url = new URL("https://metrocs.github.io/QualityToolsForBlueJ");
         assertEquals(url, unit.getURL());
     }
-}
-
-class PreferencesTest {
-
-    private Preferences unit;
-    private BlueJ bluej;
-    private JPanel myPanel;
-
-
-    @BeforeEach
-    void setup() {
-        unit = new Preferences(bluej);
-    }
-
-
 }
